a6647de089b281ee206c10c85aafb967343249a8
Moranta Fall <fall_moranta@live.fr>
2024-03-18 11:23:53 +0100

    Project setup

---
+++ src/main/java/com/moranta/collabhouse/CollabhouseApplication.java
@@ -0,0 +1,13 @@
+package com.moranta.collabhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabhouseApplication.class, args);
+	}
+
+}
